90126e4af6b41ed76a1a0b1d560bf64fd9495702
madhuptiwari <md.tiwari.9@gmail.com>
2024-08-04 13:11:11 +0530

    Project first commit

---
+++ src/test/java/com/tiwaribali/citetime/CitetimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiwaribali.citetime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitetimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
